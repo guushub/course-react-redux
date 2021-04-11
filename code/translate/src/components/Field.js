845be5a21966acabd2e697ef45aaacf5749df398
guushub <guusclaessen@gmail.com>
2021-04-11 14:41:45 +0200

    27.401

---
+++ code/translate/src/components/Field.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Field extends Component {
+    render() {
+        return (
+            <div>
+                <div className="ui field">
+                    <label>Name</label>
+                    <input />
+                </div>                
+            </div>
+        )
+    }
+}
+
+export default Field;
